2494b1d6373eb4198d4e93f6b8169b9dd6cfafc7
Jessica Adamek <jessica.g.adamek@gmail.com>
2025-02-18 15:05:42 -0300

    :art: IconButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import { ArrowRight, Copy } from "lucide-react";
+
 import { Button } from "@/components/button";
-import { ArrowRight } from "lucide-react";
+import { IconButton } from "@/components/icon-button";
 
 export default function Home() {
   return (
     <main>
-      <Button>
+      <Button type="submit">
         Enviar
         <ArrowRight />
       </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
     </main>
   );
 }
