3415e021c0df668e17ea840d29da794edbb01945
Jessica Adamek <jessica.g.adamek@gmail.com>
2025-02-19 12:20:03 -0300

    :wrench: config biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,3 @@
-import { ArrowRight, Copy, Mail } from "lucide-react";
-
-import { InputField, InputIcon, InputRoot } from "@/components/Input";
-import { Button } from "@/components/button";
-import { IconButton } from "@/components/icon-button";
-
 export default function Home() {
-  return (
-    <main>
-      <Button type="submit">
-        Enviar
-        <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail className="size-5"/>
-          </InputIcon>
-          <InputField />
-        </InputRoot>
-        <InputRoot error>
-          <InputIcon>
-            <Mail className="size-5"/>
-          </InputIcon>
-          <InputField />
-        </InputRoot>
-
-      </div>
-    </main>
-  );
+  return <main>web</main>
 }
