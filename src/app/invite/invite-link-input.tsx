4d5d86964aba6eb437789a6bec76e167c426d540
Jessica Adamek <jessica.g.adamek@gmail.com>
2025-02-19 20:53:44 -0300

    :construction: add copy link functionality

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,21 +1,28 @@
+'use client'
+
 import { InputField, InputIcon, InputRoot } from '@/components/Input'
 import { IconButton } from '@/components/icon-button'
 import { Copy, Link } from 'lucide-react'
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField
-        readOnly
-        defaultValue={'http://localhost:3000/invite/23123213212'}
-      />
+      <InputField readOnly defaultValue={inviteLink} />
 
       <IconButton className="-mr-2">
-        <Copy className="size-5" />
+        <Copy className="size-5" onClick={copyInviteLink} />
       </IconButton>
     </InputRoot>
   )
